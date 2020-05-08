68454375609feb56cf6e67a482539e82590f1ca1
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-05-08 17:22:25 +0500

    add js header

---
+++ app.js
@@ -0,0 +1,20 @@
+$(function () {
+    let header = $("#header");
+    let intro = $("#intro");
+    let introH = intro.innerHeight();
+    let scrollPos = $(window).scrollTop();
+
+    $(window).on("scroll load resize",function () {
+        let introH = intro.innerHeight();
+        scrollPos = $(this).scrollTop();
+
+        if (scrollPos > introH) {
+            header.addClass("fixed");
+        } else {
+            header.removeClass("fixed");
+        }
+
+    });
+
+    header.addClass("fixed");
+});
