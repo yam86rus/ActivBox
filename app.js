b642113c7c5b2954a532ac26859904d3759185c4
Maxim <64529642+yam86rus@users.noreply.github.com>
2020-05-20 10:47:27 +0500

    add burger menu

--- app.js
+++ app.js
@@ -1,20 +1,54 @@
 $(function () {
+    // === FIxed Header === //
     let header = $("#header");
     let intro = $("#intro");
     let introH = intro.innerHeight();
     let scrollPos = $(window).scrollTop();
+    let nav = $('#nav');
+    let navToggle = $('#navToggle');
 
-    $(window).on("scroll load resize",function () {
-        let introH = intro.innerHeight();
+    checkScroll(scrollPos,introH);
+
+    $(window).on("scroll load resize", function () {
+        introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
 
+        checkScroll(scrollPos,introH);
+        
+    });
+
+    function checkScroll(scrollPos,introH) {
         if (scrollPos > introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
+    }
+
+
+    // === Smooth scroll === //
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementId = $(this).data('scroll');
+        let elementOffset = $(elementId).offset().top;
+
+        nav.removeClass("show");
+
+        $("html, body").animate({
+            scrollTop: elementOffset - 70
+        }, 600);
 
     });
 
-    header.addClass("fixed");
+    // === Nav Toggle === //
+
+    
+    navToggle.on('click', function(event) {
+
+        event.preventDefault();
+        nav.toggleClass("show");
+
+    });
 });
